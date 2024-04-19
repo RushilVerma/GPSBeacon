279775fcc7459966fc9e33e73ac725eae8dfed40
RushilVerma <rushilverma07@gmail.com>
2024-04-19 20:55:08 +0530

    removed 3 warnings

--- app/src/main/java/com/techcravers/gpsbeacon/MainActivity.kt
+++ app/src/main/java/com/techcravers/gpsbeacon/MainActivity.kt
@@ -1,18 +1,16 @@
 package com.techcravers.gpsbeacon
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
